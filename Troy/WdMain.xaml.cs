e0cd99e31242615e05e69791a58a08b3fb85fa8a
117503445 <t117503445@gmail.com>
2019-03-25 18:14:44 +0800

    Initial commit

---
+++ Troy/WdMain.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Troy
+{
+    /// <summary>
+    /// WdMain.xaml 的交互逻辑
+    /// </summary>
+    public partial class WdMain : Window
+    {
+        public WdMain()
+        {
+            InitializeComponent();
+            App.WdMain = this;
+            Hide();
+        }
+    }
+}
